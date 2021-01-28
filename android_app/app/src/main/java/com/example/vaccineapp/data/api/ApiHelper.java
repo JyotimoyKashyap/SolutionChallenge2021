ac97cf248e84ec94e9269a6f228c6d9b40903385
Shristi Sarma <sarmashristi@hotmail.com>
2021-01-28 16:45:52 +0530

    retrofit provider added.

---
+++ android_app/app/src/main/java/com/example/vaccineapp/data/api/ApiHelper.java
@@ -0,0 +1,9 @@
+package com.example.vaccineapp.data.api;
+
+import retrofit2.Call;
+
+public class ApiHelper
+{
+
+
+}
---
+++ android_app/app/src/main/java/com/example/vaccineapp/data/api/ApiService.java
@@ -0,0 +1,5 @@
+package com.example.vaccineapp.data.api;
+
+public interface ApiService {
+
+}
