2d02d3de961773110fbeb33d9336f7c9d3612eb8
JyotimoyKashyap <jyotimoykashyap123@gmail.com>
2021-02-22 13:41:34 +0530

    1. Added api service and api helper for guide fragment

--- android_app/app/src/main/java/com/example/vaccineapp/MainDestinations/Guide/GuideFragment.java
+++ android_app/app/src/main/java/com/example/vaccineapp/MainDestinations/Guide/GuideFragment.java
@@ -4,13 +4,22 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.vaccineapp.R;
+import com.example.vaccineapp.data.Api.ApiService;
+import com.example.vaccineapp.data.Model.CovidResponse;
 import com.example.vaccineapp.databinding.FragmentGuidBinding;
 import com.google.android.material.transition.MaterialSharedAxis;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 
 public class GuideFragment extends Fragment {
@@ -25,7 +34,38 @@ public class GuideFragment extends Fragment {
         setExitTransition(new MaterialSharedAxis(MaterialSharedAxis.X, false));
         View view = binding.getRoot();
 
+        //setting retrofit for getting response from other api
+        Retrofit retrofit2 = new Retrofit.Builder()
+                .baseUrl("https://covid-19-tracking.p.rapidapi.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        ApiService apiService = retrofit2.create(ApiService.class);
+
+        Call<CovidResponse> covidResponseCall = apiService.getIndiaData();
+        covidResponseCall.enqueue(new Callback<CovidResponse>() {
+            @Override
+            public void onResponse(Call<CovidResponse> call, Response<CovidResponse> response) {
+                if(response.code() < 300){
+                    Log.d("Guide", "success");
+                }else{
+                    Log.d("Guide", "Bad Request");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<CovidResponse> call, Throwable t) {
+
+            }
+        });
+
 
         return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
--- android_app/app/src/main/java/com/example/vaccineapp/data/Api/ApiService.java
+++ android_app/app/src/main/java/com/example/vaccineapp/data/Api/ApiService.java
@@ -1,4 +1,16 @@
 package com.example.vaccineapp.data.Api;
 
+import android.content.Context;
+
+import com.example.vaccineapp.data.Model.CovidResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+
 public interface ApiService {
+
+    @Headers({"x-rapidapi-key:ec147f51cdmshd3ec85881d7532fp169b1ejsn03dfe947e1ff", "x-rapidapi-host:covid-19-tracking.p.rapidapi.com"})
+    @GET("v1/india")
+    Call<CovidResponse> getIndiaData();
 }
