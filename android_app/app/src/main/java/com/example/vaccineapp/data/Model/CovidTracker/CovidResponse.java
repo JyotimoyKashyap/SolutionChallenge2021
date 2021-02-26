66b197bb6c4b0d19c1ef338bf73c42ff84fb1bff
JyotimoyKashyap <jyotimoykashyap123@gmail.com>
2021-02-26 10:22:15 +0530

    Model classes moved from model to Model

--- android_app/app/src/main/java/com/example/vaccineapp/data/Model/CovidTracker/CovidResponse.java
+++ android_app/app/src/main/java/com/example/vaccineapp/data/Model/CovidTracker/CovidResponse.java
@@ -1,4 +1,4 @@
-package com.example.vaccineapp.data.model.CovidTracker;
+package com.example.vaccineapp.data.Model.CovidTracker;
 
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
--- android_app/app/src/main/java/com/example/vaccineapp/data/Model/ResponseVaccine.java
+++ android_app/app/src/main/java/com/example/vaccineapp/data/Model/ResponseVaccine.java
@@ -1,4 +1,4 @@
-package com.example.vaccineapp.data.model;
+package com.example.vaccineapp.data.Model;
 
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
