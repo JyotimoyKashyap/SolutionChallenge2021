2c742f6d70cb32f8df2d9c602091bc8bf93b7dce
Shristi Sarma <sarmashristi@hotmail.com>
2021-03-19 09:31:28 +0530

    Some minor changes.

--- android_app/app/src/main/java/com/example/vaccineapp/data/Model/UserDetails.java
+++ android_app/app/src/main/java/com/example/vaccineapp/data/Model/UserDetails.java
@@ -9,38 +9,24 @@ public class UserDetails {
     @Expose
     private String id;
 
-    @SerializedName("name")
-    @Expose
-    private String name;
-
-    @SerializedName("phone")
-    @Expose
-    private String phone;
-
     @SerializedName("email")
     @Expose
     private String email;
 
-    @SerializedName("address")
+    @SerializedName("uid")
     @Expose
-    private String address;
+    private String uid;
 
-    @SerializedName("__v")
+    @SerializedName("_v")
     @Expose
     private Integer _v;
 
-    @SerializedName("token")
-    @Expose
-    private String token;
 
-    public UserDetails(String id, String name, String phone, String email, String address, Integer _v, String token) {
+    public UserDetails(String id, String email,String uid, Integer _v) {
         this.id = id;
-        this.name = name;
-        this.phone = phone;
         this.email = email;
-        this.address = address;
         this._v = _v;
-        this.token = token;
+        this.uid = uid;
     }
 
     public String getId() {
@@ -51,36 +37,12 @@ public class UserDetails {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        name = name;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
     public String getEmail() {
         return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
     }
 
     public Integer get_v() {
@@ -91,11 +53,11 @@ public class UserDetails {
         this._v = _v;
     }
 
-    public String getToken() {
-        return token;
+    public String getUid() {
+        return uid;
     }
 
-    public void setToken(String token) {
-        this.token = token;
+    public void setUid(String uid) {
+        this.uid = uid;
     }
 }
