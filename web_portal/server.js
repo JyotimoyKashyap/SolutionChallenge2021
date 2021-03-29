c496100642e34adfba88780c54f9cccbb84ee5e7
Gaurav Das <gauravdas014@gmail.com>
2021-03-29 22:52:45 +0530

    docs: add webportal structure in readme

--- web_portal/server.js
+++ web_portal/server.js
@@ -4,13 +4,13 @@ dotenv.config({ path: './.env' });
 
 const app = require('./app');
 
-let DB;
-if (process.env.NODE_ENV === 'development') {
-  DB = process.env.MONGODB_LOCAL_URI;
-} else {
-  DB = process.env.MONGODB_CLOUD_URI;
-}
+const DB = process.env.MONGODB_CLOUD_URI;
 
+// if (process.env.NODE_ENV === 'development') {
+//   DB = process.env.MONGODB_LOCAL_URI;
+// } else {
+//   DB = process.env.MONGODB_CLOUD_URI;
+// }
 
 // mongo db connection using mongoose
 mongoose
